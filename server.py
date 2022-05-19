a2164f3e7ad1cb936d0c8aa842fb1565025512aa
Mauricio Collazos <ma0acollazos@gmail.com>
2022-05-18 22:07:53 -0500

    Modifying print to use last name sorting

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import requests
+from collections import OrderedDict
 
 EMPLOYEES_ENDPOINT = "https://gist.githubusercontent.com/chancock09/6d2a5a4436dcd488b8287f3e3e4fc73d/raw/fa47d64c6d5fc860fabd3033a1a4e3c59336324e/employees.json"
 
@@ -11,19 +12,43 @@ def get_org_chart():
 
 def build_tree(employee_id, children, local_managers):
     if not children:
-        return {employee_id: list()}
-    return {employee_id: list(build_tree(x, local_managers.get(x), local_managers) for x in children)}
+        return dict()
+    return {x: build_tree(x, local_managers.get(x), local_managers) for x in children}
+
 
 def print_tree(tree, employees_by_id, level):
-    for root, leafs in tree.items():
+    """
+     Michael Chen
+- Barrett Glasauer
+-- Chris Hancock
+--- Julian Early
+--- Michael Lorton
+--- Shrutika Dasgupta
+--- Mauricio Aizaga
+- Andres Green
+-- Ryan Miller
+- Natasha Prats
+-- Emily Pun
+
+    """
+    for root, leaves in tree.items():
         print(f"{'-'*level} {employees_by_id[root]['name']}")
-        for leaf in leafs:
-            print_tree(leaf, employees_by_id, level+1)
+        sorted_leaves = sorted(
+            [
+                (
+                    leaf,
+                    employees_by_id[leaf]['name'].split(" ")[1]
+                )
+                for leaf in leaves.keys()
+            ],
+            key=lambda x: x[1]
+        )
+        for leaf_key, _ in sorted_leaves:
+            print_tree({leaf_key: leaves[leaf_key]}, employees_by_id, level+1)
 
 
 def process_org_chart():
     employees_data = get_org_chart()
-    hierarchy_tree = list()
     employees_by_id = {employee.get("id"): employee for employee in employees_data}
     local_managers = dict()
     processed_employees = list()
@@ -35,15 +60,8 @@ def process_org_chart():
             local_managers[employee["manager_id"]].append(employee_id)
     tree_head = local_managers.pop(None)
 
-    # tree = build_tree(None, tree_head, local_managers)
-    # We could have multiple high level managers but for this particular example there is only one
-    tree_head = tree_head[0]
-    tree = build_tree(tree_head, local_managers[tree_head], local_managers)
-
+    tree = build_tree(None, tree_head, local_managers)
     print_tree(tree, employees_by_id, 0)
-
-
-
 
 
 if __name__ == "__main__":
